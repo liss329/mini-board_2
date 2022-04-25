adb33980c7b0e21c3f8c98dfaeb253cc2b269ab9
Lissana <liss.glace471@gmail.com>
2022-04-25 16:01:08 +0900

    feat: データベースの表示およびフォームを使った保存処理の追加
    ・7-1. DB版ミニ伝言板
    ・データベースの設定
    ・Bookshelfを用いて全ページ表示
    ・postではフォームに入力された値をmessageとしてデータベースに保存

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
+const knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: 'board_data.sqlite3',
+  },
+  useNullAsDefault: true
+});
+const Bookshelf = require('bookshelf')(knex);
+
+const Message = Bookshelf.Model.extend({
+  tableName: 'messages',
+  hasTimestamps: true,
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  new Message().fetchAll().then((collection) => {
+    const data = {
+      title: 'Express',
+      content: collection.toArray(),      
+    };
+    res.render('index', data);
+  });
+});
+
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  new Message(req.body).save().then((collection) => {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
