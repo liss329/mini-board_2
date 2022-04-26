4b1aaf92a85a1c2a62018db4f18f5239810e3996
Lissana <liss.glace471@gmail.com>
2022-04-26 18:21:59 +0900

    style: ESLintおよびPrettierの導入と適用

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,38 @@
-const { response } = require('express');
-var express = require('express');
-var router = express.Router();
-const knex = require('knex')({
-  client: 'sqlite3',
+const express = require("express");
+// eslint-disable-next-line new-cap
+const router = express.Router();
+const knex = require("knex")({
+  client: "sqlite3",
   connection: {
-    filename: 'board_data.sqlite3',
+    filename: "board_data.sqlite3",
   },
-  useNullAsDefault: true
+  useNullAsDefault: true,
 });
-const Bookshelf = require('bookshelf')(knex);
+const Bookshelf = require("bookshelf")(knex);
+Bookshelf.plugin("pagination");
 
 const Message = Bookshelf.Model.extend({
-  tableName: 'messages',
+  tableName: "messages",
   hasTimestamps: true,
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   new Message().fetchAll().then((collection) => {
     const data = {
-      title: 'Express',
+      title: "Express",
       content: collection.toArray(),      
     };
-    res.render('index', data);
+    res.render("index", data);
   });
 });
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
   console.log(req.body);
   new Message(req.body).save().then((collection) => {
-    res.redirect('/');
+    res.redirect("/");
+  });
+});
   });
 });
 
